8a062c302d317212ac61211e61c4eb503e8b4527
HishamLadha <hishamladha@gmail.com>
2025-01-29 18:04:13 -0500

    Adding onto main.py to understand SQLModel

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlmodel import Field, SQLModel, create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+# Model for the Reported Salary Table
+class ReportedSalary(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    company: str
+    year: int
+    salary: int
+    university: str
+    term: int | None = None
+    location: str
+    bonus: int | None = None
+
+
+
+# create the engine to talk to the database
+engine = create_engine(DATABASE_URL, echo=True)
+
 
 app = FastAPI()
 
