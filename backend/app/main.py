d56100465c4f01bc20fee0856ae368cfb89911bf
HishamLadha <hishamladha@gmail.com>
2025-01-29 22:07:45 -0500

    Upating the main.py file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 import os
 from dotenv import load_dotenv
 
@@ -29,56 +29,23 @@ engine = create_engine(DATABASE_URL, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-# create a model instance, basically a specific row value
-def create_test_salaries():
-    test_1 = ReportedSalary(company="ABC Test", year="1999", salary="10000", university="Concordia University", term="3", location="Montreal", bonus="0")
-
-    # create the session which takes and uses the engine provided as a parameter
-    # session = Session(engine)
-
-    # using the with block automatically creates and closes the session
-    with Session(engine) as session:
-        # add the instance to the session, currently still in memory
-        session.add(test_1)
-        # save to database now
-        session.commit()
-
-    # close the session
-    # session.close()
-
-# Create main function for code organization
-def main():
-    create_db_and_tables()
-    create_test_salaries()
-
-# this is to creat the database and tables when we run the file with "python3 main.py"
-# the internal variable __name__ in your file, created automatically by Python, will have as value the string "__main__".
-if __name__ == "__main__":
-    main()
-
-
-
 app = FastAPI()
 
-# Pydantic model for the request body
-class PostRequest(BaseModel):
-    name: str
-    address: str
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-@app.get("/test")
-def read_test():
-    return {"helloooooo"}
-
-@app.post("/posting")
-def post_test(request: PostRequest):
-    return {
-        "message": "Received successfully",
-        "data": {
-            "name": request.name,
-            "address": request.address
-        },
-    }
+@app.post("/submit-salary")
+def create_salary(reportedSalary: ReportedSalary):
+    with Session(engine) as session:
+        session.add(reportedSalary)
+        session.commit()
+        session.refresh(reportedSalary)
+        return reportedSalary
+    
+@app.get("/all-salaries")
+def read_salaries():
+    with Session(engine) as session:
+        salaries = session.exec(select(ReportedSalary)).all()
+        return salaries
