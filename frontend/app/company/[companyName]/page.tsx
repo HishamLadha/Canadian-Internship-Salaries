a4d74e4c18a79739d70d578e554a26cd6b3b0122
HishamLadha <hishamladha@gmail.com>
2025-02-03 21:54:47 -0500

    Added useEffect calls for /company

--- frontend/app/company/[companyName]/page.tsx
+++ frontend/app/company/[companyName]/page.tsx
@@ -8,23 +8,68 @@ import {
 import { CompanyTable } from "@/components/custom/companyTable";
 
 import { useParams } from 'next/navigation'
+import { useEffect, useState } from "react";
  
 export default function Company() {
   const params = useParams();
   const { companyName } = params;
+  const [decodedCompanyName, setDecodedCompanyName] = useState('');
+
+  // Decode the companyName when it becomes available or changes.
+  useEffect(() => {
+    if (companyName) {
+      // Ensure companyName is a string.
+      const safeCompanyName = Array.isArray(companyName)
+        ? companyName[0]
+        : companyName;
+      try {
+        const decoded = decodeURIComponent(safeCompanyName);
+        setDecodedCompanyName(decoded);
+      } catch (error) {
+        console.error("Error decoding companyName:", error);
+        setDecodedCompanyName(safeCompanyName);
+      }
+    }
+  }, [companyName]);
+
+  // Fetch company data from API when component is loaded
+  useEffect(() => {
+    if (!decodedCompanyName){
+      return
+    }
+    const fetchCompanyData = async (decodedCompanyName: string) => {
+      try{
+        const response = await fetch(`http://localhost:8000/company?company=${decodedCompanyName}`)
+        if(!response.ok){
+          throw new Error("Failed to fetch this companies data")
+        }
+        const data = await response.json();
+        console.log(data)
+
+      }
+      catch{
+
+      }finally{
+
+      }
+
+    }
+
+    fetchCompanyData(decodedCompanyName)
+  }, [decodedCompanyName])
 
   return (
   // <p>Post: {companyName}</p>
     <div className='mt-6'>
       {/* Text description of company */}
-      <div className='flex justify-center'>
+      <div className='flex justify-center text-center'>
         <h1 className='text-4xl 
                 sm:text-5xl 
                 font-extrabold 
                 tracking-tight 
                 text-gray-900 
                 bg-clip-text'>
-                   {companyName}
+                   {decodedCompanyName}
         </h1>
      
       </div>
